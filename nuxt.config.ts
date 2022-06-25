6ca2c880faf3208d9ac21f8ed0c859c791514baa
Ewa Utracka <ewa.utr@gmail.com>
2022-06-25 12:37:17 +0200

    confix configured

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,38 @@
 import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
+import svgLoader from 'vite-svg-loader'
 
+export default defineNuxtConfig({
+    app: {
+      head: {
+        title: 'Hacker News',
+        htmlAttrs: {
+          lang: 'en',
+        },
+        meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          { hid: 'description', name: 'description', content: '' },
+          { name: 'format-detection', content: 'telephone=no' },
+        ],
+      }
+    },
+
+    css: ['~/assets/styles/main.scss'],
+
+    vite: {
+      plugins: [
+        svgLoader({
+          /* ... */
+        })
+      ],
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: '@import "@/assets/styles/variables.scss"; @import "@/assets/styles/mixins.scss";',
+          },
+        },
+      },
+    },
 })
