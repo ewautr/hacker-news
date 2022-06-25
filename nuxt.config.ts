6adfbc04d8760b68338138fe194846e5099d0bad
Ewa Utracka <ewa.utr@gmail.com>
2022-06-25 13:00:26 +0200

    favicon added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,7 @@ export default defineNuxtConfig({
           { hid: 'description', name: 'description', content: '' },
           { name: 'format-detection', content: 'telephone=no' },
         ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
       }
     },
 
